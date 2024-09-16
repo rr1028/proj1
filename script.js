3b5baf427f019d01c4b5981e5dcef6096e297fed
rishi <104455207+rr1028@users.noreply.github.com>
2024-09-16 10:14:09 +0530

    Add files via upload
    c1

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    // Get the user input values
+    const name = document.getElementById("name").value;
+    const bloodType = document.getElementById("bloodType").value;
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const allergies = document.getElementById("allergies").value;
+  
+    // Store the input data in localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("bloodType", bloodType);
+    localStorage.setItem("height", height);
+    localStorage.setItem("weight", weight);
+    localStorage.setItem("allergies", allergies);
+  
+    // Redirect to index.html
+    window.location.href = "login.html";
+  });
+  
